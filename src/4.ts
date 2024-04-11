d79a47c94b8d51312ae67984df5117e102d654af
MaxKov86 <max.kov86@ukr.net>
2024-04-11 21:43:44 +0300

    Updated 4.ts

--- src/4.ts
+++ src/4.ts
@@ -6,19 +6,19 @@ class Key {
 }
 
 class Person {
-  private key: number;
+  private key: Key;
   constructor(key) {
     this.key = key;
   }
-  getKey(): number {
+  getKey(): Key {
     console.log(this.key);
     return this.key;
   }
 }
 
 abstract class House {
-  protected door: boolean;
-  private tenants: string[] = [];
+  protected door: boolean = false;
+  tenants: string[] = [];
 
   constructor(public key: Key) {}
 
@@ -33,9 +33,9 @@ abstract class House {
 
 class MyHouse extends House {
   openDoor(key): void {
-    if (this.key.getSignature() === key) {
+    if (this.key.getSignature() === key.getSignature()) {
       this.door = true;
-      console.log(`Welcome houme`);
+      console.log("Welcome houme");
     } else {
       console.log("Wrong key");
     }
